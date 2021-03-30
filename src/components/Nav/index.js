71c7119cf926e3842a831f1d9c5f8983eef94bb7
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-03-29 21:57:38 -0600

    added Nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,9 +22,8 @@ function Nav() {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
-            {" "}
             📸
           </span>{" "}
           Oh Snap!
@@ -33,7 +32,9 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a data-testid="about" href="#about">
+              About Me
+            </a>
           </li>
           <li>
             <span>Contact</span>
